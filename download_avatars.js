b8528b3d3ae3567c1dda18acea0cb032754382cd
martincapo <martincapo@yahoo.com>
2017-09-11 22:27:34 +0000

    Step3

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,30 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+// var GITHUB_USER = "martincapo";
+// var GITHUB_TOKEN = "YOUR ACCESSTOKEN HERE";
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+  request(url, function(error, response, body) {
+    // For error
+    if(error) {
+      cb(error);
+      return;
+    }
+    if(response && response.statusCode === 200) {
+      var json = JSON.parse(body);
+      cb(null, json.data);
+    } else if(response.statusCode === 404) {
+      cb("Page Not found");
+    }
+
+  })
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
