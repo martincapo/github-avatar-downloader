f66e52bc7df3fbfddb214594db32108a1be16356
martincapo <martincapo@yahoo.com>
2017-09-11 22:57:20 +0000

    Step5

--- download_avatars.js
+++ download_avatars.js
@@ -6,23 +6,23 @@ var GITHUB_USER = "martincapo";
 var GITHUB_TOKEN = "37494239f2d8c06f046ede463765fe54cd3bc1d5";
 
 
-
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-
-  request(requestURL, function(error, response, body) {
+  console.log(requestURL);
+  request(requestURL, function(error, response) {
     // For error
     if(error) {
       cb(error);
       return;
     }
-    if(response && response.statusCode === 200) {
-      var json = JSON.parse(body);
-      cb(null, json.data);
-    } else if(response.statusCode === 404) {
-      cb("Page Not found");
-    }
+
+//    console.log("I am here!!!", response);
+
+   // var json = JSON.parse(body);
+   // cb(null, json.data);
+    cb(null, response);
+
 
   })
 }
